9efebc09a2bf3f0359cc1d04b3cd8fb53c835689
papa-marshmellow <logan.t2020@tutanota.com>
2022-08-04 18:36:10 -0400

    added jackd support and made pipewire the default audio server

--- configuration.nix
+++ configuration.nix
@@ -76,10 +76,14 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
+    jack = {
+      enable = true;
+      jackd.enable = true;
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
