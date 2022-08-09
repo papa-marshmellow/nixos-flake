e256301e698e225e65fc9c4ea6bea618264280db
papa-marshmellow <logan.t2020@tutanota.com>
2022-08-09 00:23:34 -0400

    aligned mangohud to top center for terraria

--- modules/mangohud.nix
+++ modules/mangohud.nix
@@ -4,28 +4,46 @@
   programs.mangohud = {
     enable = true;
     settings = {
-      background_alpha = "0.3";
+      background_alpha = "0.4";
       font_size = "20";
       background_color = "002B36";
       text_color = "FDF6E3";
-      position = "top-right";
-      no_display = true;
+      position = "top-left";
       toggle_hud = "F11";
-      cpu_stats = true;
+      time = true;
+      time_format = "%a %l:%M %P";
+      media_player = true;
+      battery_icon = true;
       cpu_temp = true;
       cpu_color = "2AA198";
-      gpu_stats = true;
       gpu_temp = true;
       gpu_color = "859900";
       ram = true;
       ram_color = "D33682";
-      engine_color = "B58900";
-      frame_timing = 1;
-      frametime_color = "839496";
+      frame_timing= false;
     };
     settingsPerApplication = {
       mpv = {
         no_display = false;
+      };
+      "Terraria.bin.x86_64" = {
+        background_alpha = "0.4";
+        font_size = "20";
+        background_color = "002B36";
+        text_color = "FDF6E3";
+        position = "top-center";
+        toggle_hud = "F11";
+        time = true;
+        time_format = "%a %l:%M %P";
+        media_player = true;
+        battery_icon = true;
+        cpu_temp = true;
+        cpu_color = "2AA198";
+        gpu_temp = true;
+        gpu_color = "859900";
+        ram = true;
+        ram_color = "D33682";
+        frame_timing= false;
       };
     };
   };
