126bbd3cf13fdfdc802d008d6095bd67f0f09fd2
papa-marshmellow <logan.t2020@tutanota.com>
2022-08-16 12:54:27 -0400

    updated homemanager url in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
