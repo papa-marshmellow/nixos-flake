7910907110e63ca2bed894e6914cee8f39b7fb93
papa-marshmellow <logan.t2020@tutanota.com>
2022-09-04 22:26:24 -0400

    fixed urxvt module for real

--- modules/urxvt.nix
+++ modules/urxvt.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }
+{ pkgs, ... }:
 
 {
 
@@ -8,25 +8,32 @@
       "Shift-Control-C" = "eval:selection_to_clipboard";
       "Shift-Control-V" = "eval:paste_clipboard";
     };
-    transparent = true;
     fonts = [ "xft:Ubuntu Mono:pixelsize=28" ];
+    scroll.bar.enable = false;
     extraConfig = {
       depth = 32;
       letterSpace = -1;
-      urlLauncher = librewolf;
-      cursorColor = #FDF6E3;
+      urlLauncher = "librewolf";
+      cursorColor = "#FDF6E3";
       internalBorder = 15;
-      background = [75]#002b36;
-      foreground = #FDF6E3;
-      !! light colors
-      color8 = #002b3b ! black
-      color9 = #CB4B16 ! red
-      color10 = #859900 ! green
-      color11 = #B58900 ! yellow
-      color12 = #268BD2 ! blue
-      color13 = #D33682 ! magenta
-      color14 = #2AA198 ! cyan
-      color15 = #FDF6E3 ! white
+      background = "[75]#002B36";
+      foreground = "#FDF6E3";
+      color0 = "#002b3b";
+      color8 = "#002b3b";
+      color1 = "#CB4B16";
+      color9 = "#CB4B16";
+      color2 = "#859900";
+      color10 = "#859900";
+      color3 = "#B58900";
+      color11 = "#B58900";
+      color4 = "#268BD2";
+      color12 = "#268BD2";
+      color5 = "#D33682";
+      color13 = "#D33682";
+      color6 = "#2AA198";
+      color14 = "#2AA198";
+      color7 = "#FDF6E3";
+      color15 = "#FDF6E3";
     };
   };
 }
