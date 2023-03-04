8e69d9c4016a0e459c6a55d64c5957b02e342759
lowgain <logan.t2020@tutanota.com>
2023-03-04 15:59:35 -0500

    remove wireguard

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -18,23 +18,6 @@
   networking = {
     hostName = "frying-pan";
     wireless.iwd.enable = true;
-    wg-quick.interfaces = {
-      mom-house = {
-        address = [ "10.13.13.3/24" ];
-        dns = [ "10.13.13.1" ];
-        privateKeyFile = "/etc/wireguard/mom-house/private.key";
-        listenPort = [ 51820 ];
-        peers = [
-          {
-            publicKey = "29Sy3/sbHuBiI8xDSzRmwN0RTMjb9DsfeHwO4NNb5lY=";
-            presharedKeyFile = "/etc/wireguard/mom-house/preshared.key";
-            allowedIPs = [ "0.0.0.0/0" "::/0" ];
-            endpoint = "joneserconvention.com:51820";
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
   };
 
   # Mullvad VPN
