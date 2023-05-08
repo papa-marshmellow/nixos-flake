a3a6486573819b4561235664225a4573f337c96f
lowgain <logan.t2020@tutanota.com>
2023-05-08 11:28:36 -0400

    refactor consolidate pkgs

--- hosts/vm/home.nix
+++ hosts/vm/home.nix
@@ -1,38 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  imports = [( import ../../modules )];
-
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "lowgain";
-  home.homeDirectory = "/home/lowgain";
-
-  home.stateVersion = "23.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  # Enable unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # Install applications
-  home.packages = with pkgs; [
-  ];
+  # home.packages = with pkgs; [
+  # ];
 
-  xsession = {
-    windowManager = {
-      bspwm = {
-        startupPrograms = [
-          "polybar mybar -r"
-          "pa-applet"
-        ];
-        monitors = {
-          Virtual-1 = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" ];
-        };
-      };
-    };
-  };
+  xsession.windowManager.bspwm.monitors.Virtual-1 = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" ];
 
   services.picom = {
     vSync = false;
