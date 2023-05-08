c550edac1d9a23a143f6aae8b9b7d4fb6552eeb8
lowgain <logan.t2020@tutanota.com>
2023-05-08 15:24:32 -0400

    add hyprland cachix

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -84,7 +84,15 @@
   nixpkgs.config.allowUnfree = true;
 
   nix = {
-    settings.auto-optimise-store = true;
+    settings = {
+      auto-optimise-store = true;
+      substituters = [
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
     optimise.automatic = true;
     gc = {
       automatic = true;
